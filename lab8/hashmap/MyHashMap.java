de88ed4a917bb862eff2a878fd7960f790bc665c
Enokimushroom <372339878@qq.com>
2022-03-18 17:08:51 +0800

    done with lab8

--- lab8/hashmap/MyHashMap.java
+++ lab8/hashmap/MyHashMap.java
@@ -1,6 +1,6 @@
 package hashmap;
 
-import java.util.Collection;
+import java.util.*;
 
 /**
  *  A hash table-backed Map implementation. Provides amortized constant time
@@ -26,13 +26,20 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     }
 
     /* Instance Variables */
+    private static int CAPACITY = 16;
+    private static double MAX_LOAD = 0.75;
+    private int size;
     private Collection<Node>[] buckets;
     // You should probably define some more!
 
     /** Constructors */
-    public MyHashMap() { }
+    public MyHashMap() {
+        this(CAPACITY, MAX_LOAD);
+    }
 
-    public MyHashMap(int initialSize) { }
+    public MyHashMap(int initialSize) {
+        this(initialSize, MAX_LOAD);
+    }
 
     /**
      * MyHashMap constructor that creates a backing array of initialSize.
@@ -41,13 +48,18 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param initialSize initial size of backing array
      * @param maxLoad maximum load factor
      */
-    public MyHashMap(int initialSize, double maxLoad) { }
+    public MyHashMap(int initialSize, double maxLoad) {
+        CAPACITY = initialSize;
+        MAX_LOAD = maxLoad;
+        size = 0;
+        buckets = createTable(CAPACITY);
+    }
 
     /**
      * Returns a new node to be placed in a hash table bucket
      */
     private Node createNode(K key, V value) {
-        return null;
+        return new Node(key, value);
     }
 
     /**
@@ -69,7 +81,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
      */
     protected Collection<Node> createBucket() {
-        return null;
+        return new LinkedList<>();
     }
 
     /**
@@ -82,10 +94,192 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param tableSize the size of the table to create
      */
     private Collection<Node>[] createTable(int tableSize) {
+        Collection<Node>[] table = new Collection[tableSize];
+        for (int i = 0; i < tableSize; i++) {
+            table[i] = createBucket();
+        }
+        return table;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+        buckets = createTable(CAPACITY);
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
+        return getNode(buckets, key) != null;
+    }
+
+    private Node getNode(Collection<Node>[] buckets, K key) {
+        if (buckets == null || key == null) {
+            throw new IllegalArgumentException();
+        }
+        int i = hash(key);
+        for (Node node : buckets[i]) {
+            if (node.key.equals(key)) {
+                return node;
+            }
+        }
         return null;
     }
 
-    // TODO: Implement the methods of the Map61B Interface below
-    // Your code won't compile until you do so!
+    private int hash(K key) {
+        return hash(buckets, key);
+    }
+
+    private int hash(Collection<Node>[] buckets, K key) {
+        return Math.floorMod(key.hashCode(), buckets.length);
+    }
+
+    @Override
+    public V get(K key) {
+        return get(buckets, key);
+    }
+
+    private V get(Collection<Node>[] buckets, K key) {
+        if (buckets == null || key == null) {
+            throw new IllegalArgumentException();
+        }
+        Node node = getNode(buckets, key);
+        if (node != null) {
+            return node.value;
+        }
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
+        double load = (double) size / buckets.length;
+        if (load >= MAX_LOAD) {
+            resize(buckets.length * 2);
+        }
+        Node node = getNode(buckets, key);
+        if (node == null) {
+            int i = hash(key);
+            buckets[i] = put(buckets[i], key, value);
+        } else {
+            node.value = value;
+        }
+    }
+
+    private Collection<Node> put(Collection<Node> bucket, K key, V value) {
+        Node node = createNode(key, value);
+        bucket.add(node);
+        size++;
+        return bucket;
+    }
+
+    private void resize(int capacity) {
+        Collection<Node>[] newBuckets = createTable(capacity);
+        Iterator<Node> nodeIter = new nodeIterator();
+        while (nodeIter.hasNext()) {
+            Node node = nodeIter.next();
+            int i = hash(newBuckets, node.key);
+            newBuckets[i].add(node);
+        }
+        buckets = newBuckets;
+    }
+
+    @Override
+    public Set<K> keySet() {
+        Set<K> set = new HashSet<>();
+        Iterator<Node> nodeIter = new nodeIterator();
+        while (nodeIter.hasNext()) {
+            Node node = nodeIter.next();
+            set.add(node.key);
+        }
+        return set;
+    }
+
+    @Override
+    public V remove(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
+        Node node = getNode(buckets, key);
+        if (node == null) {
+            return null;
+        }
+        V val = node.value;
+        int i = hash(key);
+        buckets[i].remove(node);
+        return val;
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
+        Node node = getNode(buckets, key);
+        if (node == null) {
+            return null;
+        }
+        V val = node.value;
+        int i = hash(key);
+        if (val.equals(value)) {
+            buckets[i].remove(node);
+            return val;
+        }
+        return null;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return new MyhashmapIter();
+    }
+
+    private class MyhashmapIter implements Iterator<K> {
+
+        private nodeIterator nodeIter = new nodeIterator();
+
+        @Override
+        public boolean hasNext() {
+            return nodeIter.hasNext();
+        }
+
+        @Override
+        public K next() {
+            return nodeIter.next().key;
+        }
+    }
+
+    private class nodeIterator implements Iterator<Node> {
+
+        private final Iterator<Collection<Node>> bucketIter = Arrays.stream(buckets).iterator();
+        private Iterator<Node> curIter;
+        private int nodeNum = size;
+
+        @Override
+        public boolean hasNext() {
+            return nodeNum > 0;
+        }
+
+        @Override
+        public Node next() {
+            if (curIter == null  || !curIter.hasNext()) {
+                Collection<Node> curBucket = bucketIter.next();
+                while (curBucket.size() == 0) {
+                    curBucket = bucketIter.next();
+                }
+                curIter = curBucket.iterator();
+            }
+            nodeNum--;
+            return curIter.next();
+        }
+    }
 
 }
